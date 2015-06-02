47f99ee819c6b6868bba972ab0148a4b4e0c1da0
Patrick Bogucki <patrickbogucki@gmail.com>
2015-06-02 15:06:22 -0700

    Player must wait for animation of pieces appearing before placing their piece on the board. Other wise they get a message at the bottom.

--- scripts.js
+++ scripts.js
@@ -6,24 +6,37 @@ function main() {
 	];
 	var playerHasCurrentTurn = true;
 	var hasPieceMsg = "There is already a piece there.";
+	var waitMsg = "Please Wait";
+	var appeared;
 
 	$('.cell').on('click', function() {
 		if(containsPiece($(this))) {
 			$('.alert-message').text(hasPieceMsg);
 			$('.alert-message').finish().css('display', 'none').fadeIn('slow').delay(1000).fadeOut('slow', function() {
  			});
+		} else if(appeared === false) {
+			$('.alert-message').text(waitMsg);
+			$('.alert-message').finish().css('display', 'none').fadeIn('slow').delay(1000).fadeOut('slow', function() {
+ 			});
 		} else {
-		$(this).children().remove();
-		if(playerHasCurrentTurn) {
-			$(this).append('<div class="piece-x"></div>');
-			$(this).find('.piece-x').fadeIn('slow', function() {});
-			playerHasCurrentTurn = false;
-		} else {
-			$(this).append('<div class="piece-o"></div>');
-			$(this).find('.piece-o').fadeIn('slow', function() {});
-			playerHasCurrentTurn = true;
+			$(this).children().remove();
+			appeared = false;
+			if(playerHasCurrentTurn) {
+				$(this).append('<div class="piece-x"></div>');
+				$(this).find('.piece-x').fadeIn(1000, function() {
+					appeared = true;
+				});
+				
+				playerHasCurrentTurn = false;
+			} else {
+				$(this).append('<div class="piece-o"></div>');
+				$(this).find('.piece-o').fadeIn(1000, function() {
+					appeared = true;
+				});
+
+				playerHasCurrentTurn = true;
+			}
 		}
-	}
 	});
 
 	function containsPiece(cell) {
