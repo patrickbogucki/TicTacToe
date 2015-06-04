b8f288a24519b5485fbb31e7f86f8c578b7c5ccf
squirrelxseeker <patrickbogucki@gmail.com>
2015-06-04 01:17:48 -0700

    Added a game object to track board and perform calculations and check game state. Refactored some functions that could be done by game object. Checks if game is over and displays message.

--- scripts.js
+++ scripts.js
@@ -1,46 +1,43 @@
 function main() {
 
-	var pieceXClass = "x";
-	var pieceOClass = "o";
+	var pieceX = "x";
+	var pieceO = "o";
 
-	var boardArray = [
-		[null, null, null],
-		[null, null, null],
-		[null, null, null]
-	];
-	var currentPiece = pieceXClass; // X starts with first move
+	var currentPiece = pieceX; 	// X starts with first move
+
 	var hasPieceMsg = "There is already a piece there.";
 	var waitMsg = "Please Wait";
+	var gameOverMsg = "Game Over.\nRefresh to play again";
+	
 	var appeared;
+
+	var game = new Game();
 
 	$('.cell').on('click', function() {
 		var clickedCell = $(this);
-		if(containsPiece(clickedCell)) {
-			drawAlertMessage(hasPieceMsg);
-		} else if(appeared === false) {
-			drawAlertMessage(waitMsg);
+		var rowIndex = clickedCell.closest('.row').index('.row');
+		var colIndex = clickedCell.index();
+
+		if(game.isGameOver) {
+			drawAlertMessage(gameOverMsg);
 		} else {
-			clickedCell.children().remove();
-			appeared = false;
+			if(game.cellContainsPiece(rowIndex, colIndex)) {
+				drawAlertMessage(hasPieceMsg);
+			} else if(appeared === false) {
+				drawAlertMessage(waitMsg);
+			} else {
+				clickedCell.children().remove();
+				appeared = false;
 
-			// Log location of click on board
-			var rowIndex = clickedCell.closest('.row').index('.row');
-			var colIndex = clickedCell.index();
-			boardArray[rowIndex][colIndex] = currentPiece;
+				// Log location of click on board
+				game.updateBoardArray(rowIndex, colIndex, currentPiece);
+				game.printBoardArray();
 
-			drawPiece(clickedCell, currentPiece);
-			
-			// Print out board array
-			console.log("");
-			var line = "";
-			for(var i = 0; i < boardArray.length; i++) {
-				for(var j = 0; j < boardArray[i].length; j++) {
-					line = line + boardArray[i][j] + ', ';
-				}
-				console.log(line);
-				line = "";
+				drawPiece(clickedCell, currentPiece);
+
+				game.checkGameForWin(rowIndex, colIndex);
 			}
-		}
+		}		
 	});
 
 	function drawAlertMessage(message) {
@@ -53,30 +50,65 @@ function main() {
 			});
 	}
 
-	function containsPiece(cell) {
-		if(cell.children().length > 0) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
 	function drawPiece(cell, pieceSymbol) {
 		cell.append('<div class=piece-' + pieceSymbol + '></div>');
 		cell.find('.piece-' + pieceSymbol).fadeIn('slow', function() {
 			appeared = true;
 		});
 		
-		if(currentPiece === pieceXClass) {
-			currentPiece = pieceOClass;
+		if(currentPiece === pieceX) {
+			currentPiece = pieceO;
 			$('.playerHeader').css('font-weight', 300);
 			$('.AIHeader').css('font-weight', 400);
 		} else {
-			currentPiece = pieceXClass;
+			currentPiece = pieceX;
 			$('.playerHeader').css('font-weight', 400);
 			$('.AIHeader').css('font-weight', 300);
 		}
 	}
 }
+
+function Game() {
+	this.boardArray = [
+		[null, null, null],
+		[null, null, null],
+		[null, null, null]
+	];
+	this.isGameOver = false;
+
+	console.log("New game started.");
+}
+
+Game.prototype.updateBoardArray = function(row, col, currentPiece) {
+	this.boardArray[row][col] = currentPiece;
+};
+
+Game.prototype.printBoardArray = function() {
+	console.log("");
+	var line = "";
+	for(var i = 0; i < this.boardArray.length; i++) {
+		for(var j = 0; j < this.boardArray[i].length; j++) {
+			line = line + this.boardArray[i][j] + ', ';
+		}
+		console.log(line);
+		line = "";
+	}
+};
+
+Game.prototype.cellContainsPiece = function(row, col) {
+	return (this.boardArray[row][col] !== null);
+};
+
+Game.prototype.checkGameForWin = function(row, col) {
+	console.log("Checking if last move was a win");
+	for(var i = 0; i < this.boardArray.length; i++) {
+		for(var j = 0; j < this.boardArray[i].length; j++) {
+			if(this.boardArray[i][j] === null) {
+				return false;
+			}
+		}
+	}
+	this.isGameOver = true;
+};
 
 $(document).ready(main);
