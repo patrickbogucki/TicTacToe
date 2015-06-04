33f469103a0ef4f9631071a6305289f214c189de
Patrick Bogucki <patrickbogucki@gmail.com>
2015-06-03 23:40:50 -0700

    Refactored repeating steps into functions. Functioning array that logs placement of pieces so as to begin looking for wins and creating an AI.

--- scripts.js
+++ scripts.js
@@ -1,49 +1,80 @@
 function main() {
+
+	var pieceXClass = "x";
+	var pieceOClass = "o";
+
 	var boardArray = [
-		[false, false, false],
-		[false, false, false],
-		[false, false, false]
+		[null, null, null],
+		[null, null, null],
+		[null, null, null]
 	];
-	var playerHasCurrentTurn = true;
+	var currentPiece = pieceXClass; // X starts with first move
 	var hasPieceMsg = "There is already a piece there.";
 	var waitMsg = "Please Wait";
 	var appeared;
 
 	$('.cell').on('click', function() {
-		if(containsPiece($(this))) {
-			$('.alert-message').text(hasPieceMsg);
-			$('.alert-message').finish().css('display', 'none').fadeIn('slow').delay(1000).fadeOut('slow', function() {
- 			});
+		var clickedCell = $(this);
+		if(containsPiece(clickedCell)) {
+			drawAlertMessage(hasPieceMsg);
 		} else if(appeared === false) {
-			$('.alert-message').text(waitMsg);
-			$('.alert-message').finish().css('display', 'none').fadeIn('slow').delay(1000).fadeOut('slow', function() {
- 			});
+			drawAlertMessage(waitMsg);
 		} else {
-			$(this).children().remove();
+			clickedCell.children().remove();
 			appeared = false;
-			if(playerHasCurrentTurn) {
-				$(this).append('<div class="piece-x"></div>');
-				$(this).find('.piece-x').fadeIn(1000, function() {
-					appeared = true;
-				});
-				
-				playerHasCurrentTurn = false;
-			} else {
-				$(this).append('<div class="piece-o"></div>');
-				$(this).find('.piece-o').fadeIn(1000, function() {
-					appeared = true;
-				});
 
-				playerHasCurrentTurn = true;
+			// Log location of click on board
+			var rowIndex = clickedCell.closest('.row').index('.row');
+			var colIndex = clickedCell.index();
+			boardArray[rowIndex][colIndex] = currentPiece;
+
+			drawPiece(clickedCell, currentPiece);
+			
+			// Print out board array
+			console.log("");
+			var line = "";
+			for(var i = 0; i < boardArray.length; i++) {
+				for(var j = 0; j < boardArray[i].length; j++) {
+					line = line + boardArray[i][j] + ', ';
+				}
+				console.log(line);
+				line = "";
 			}
 		}
 	});
+
+	function drawAlertMessage(message) {
+		$('.alert-message').text(message);
+		$('.alert-message').finish()
+			.css('display', 'none')
+			.fadeIn('slow')
+			.delay(1000)
+			.fadeOut('slow', function() {
+			});
+	}
 
 	function containsPiece(cell) {
 		if(cell.children().length > 0) {
 			return true;
 		} else {
 			return false;
+		}
+	}
+
+	function drawPiece(cell, pieceSymbol) {
+		cell.append('<div class=piece-' + pieceSymbol + '></div>');
+		cell.find('.piece-' + pieceSymbol).fadeIn('slow', function() {
+			appeared = true;
+		});
+		
+		if(currentPiece === pieceXClass) {
+			currentPiece = pieceOClass;
+			$('.playerHeader').css('font-weight', 300);
+			$('.AIHeader').css('font-weight', 400);
+		} else {
+			currentPiece = pieceXClass;
+			$('.playerHeader').css('font-weight', 400);
+			$('.AIHeader').css('font-weight', 300);
 		}
 	}
 }
